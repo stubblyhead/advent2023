e39feadc226c79fe303d41cab353bfc73e25bb63
stubblyhead <27823133+stubblyhead@users.noreply.github.com>
2023-12-01 14:54:15 -0800

    day1 testcase passing

---
+++ day1/day1.py
@@ -0,0 +1,12 @@
+import re
+
+lines = open('testcase').readlines(-1)
+calsum = 0
+
+for l in lines:
+    pattern = re.compile('\d')
+    digits = pattern.findall(l)
+    calsum += int(digits[0]) * 10 + int(digits[-1])
+
+print(calsum)
+    
