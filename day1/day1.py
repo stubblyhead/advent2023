6e21b42e0113c5f14e2ca502af914b74ac7b5e29
stubblyhead <27823133+stubblyhead@users.noreply.github.com>
2023-12-01 15:33:06 -0800

    day 2 test passing

--- day1/day1.py
+++ day1/day1.py
@@ -1,12 +1,57 @@
 import re
 
-lines = open('input').readlines(-1)
+lines = open('testcase').readlines(-1)
 calsum = 0
 
 for l in lines:
-    pattern = re.compile('\d')
+    pattern = re.compile('\d')  #compiling since we're doing this a lot
     digits = pattern.findall(l)
-    calsum += int(digits[0]) * 10 + int(digits[-1])
+    calsum += int(digits[0]) * 10 + int(digits[-1]) #first match is the 10s digit
 
 print(calsum)
-    
+
+
+lines = open('testcase2').readlines(-1)
+calsum = 0  #i am surprised I did not forget to do this
+
+def word_to_int(word):
+    if word == 'one':
+        return 1
+    elif word == 'two':
+        return 2
+    elif word == 'three':
+        return 3
+    elif word == 'four':
+        return 4
+    elif word == 'five':
+        return 5
+    elif word == 'six':
+        return 6
+    elif word == 'seven':
+        return 7
+    elif word == 'eight':
+        return 8
+    elif word == 'nine':
+        return 9
+    elif word == 'zero':
+        return 0
+
+for l in lines:
+    pattern = re.compile('\d|one|two|three|four|five|six|seven|eight|nine|zero')
+    #alternatively could s// words for digits?  not sure which would be faster
+
+    digits = pattern.findall(l)
+    if len(digits[0]) > 1:
+        firstnum = word_to_int(digits[0])
+    else:
+        firstnum = int(digits[0])
+
+    if len(digits[-1]) > 1:
+        lastnum = word_to_int(digits[-1])
+    else:
+        lastnum = int(digits[-1])
+
+    calsum += firstnum * 10 + lastnum
+
+print(calsum)
+
