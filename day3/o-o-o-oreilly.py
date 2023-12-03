11759265117bc1bde54b0c512317dd49856e74c9
stubblyhead <27823133+stubblyhead@users.noreply.github.com>
2023-12-03 13:53:36 -0800

    getting characters surrounding each number

---
+++ day3/o-o-o-oreilly.py
@@ -0,0 +1,31 @@
+import regex as re
+lines = open('testcase').readlines(-1)
+partsum = 0
+
+for i in range(len(lines)):
+    cur = lines[i].strip()
+    if i == 0:
+        prev = ''
+    else:
+        prev = lines[i-1].strip()
+    if i == len(lines)-1:
+        next = ''
+    else:
+        next = lines[i+1].strip()
+    pattern = re.compile('\d+')
+    numbers = pattern.findall(cur)  # find all numbers in each line
+    for n in numbers:
+        start = cur.index(n)  # where does each nubmer begin
+        length = len(n)  # how long is it
+        border = ''
+        left_edge = max(0,start-1)
+        right_edge = min(start+length+1,len(cur)-1)
+        
+        
+        border += prev[left_edge:right_edge] # get characters above number
+        border += next[left_edge:right_edge] # get chars below number
+        if start > 0:
+            border += cur[start-1]
+        if start + length < len(cur):
+            border += cur[start+length]
+        print(n, border)
