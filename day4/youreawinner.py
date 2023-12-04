d18763378917f26702c586b7cae2daf8a2cfc36b
stubblyhead <27823133+stubblyhead@users.noreply.github.com>
2023-12-04 00:18:13 -0800

    shirley jackson, where are you?

--- day4/youreawinner.py
+++ day4/youreawinner.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 lines = open('input').readlines()
 points = 0
 
@@ -14,3 +15,21 @@ for card in lines:
         points += 2**(matches-1)
 
 print(points)
+
+# this is the dumbest lottery ever
+cardcounts = defaultdict(int)
+for i in range(len(lines)):
+    card = lines[i]
+    (winners, picks) = card.split(' | ')
+    winners = winners.split(': ')[1].strip().split()
+    picks = picks.strip().split()
+    matches = 0
+    cardcounts[i] += 1 # always add one for the original card
+    for p in picks:
+        if p in winners:
+            matches += 1
+    if matches > 0:
+        for j in range(i + 1, i + matches + 1):  # for the ${matches} next cards...
+            cardcounts[j] += cardcounts[i]  # ... add equal to the count of the current card
+
+print(sum(cardcounts.values()))
