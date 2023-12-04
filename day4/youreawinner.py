ce733af68268f4b96a2036414ef8b67eb8d7a221
stubblyhead <27823133+stubblyhead@users.noreply.github.com>
2023-12-03 23:51:59 -0800

    too much splits (part 1 compelte)

--- day4/youreawinner.py
+++ day4/youreawinner.py
@@ -1,11 +1,11 @@
-lines = open('testcase').readlines()
+lines = open('input').readlines()
 points = 0
 
 for card in lines:
     # the card number will probably be significant in part 2
     (winners, picks) = card.split(' | ')
-    winners = winners.split(': ')[1].strip().split(' ')
-    picks = picks.strip().split(' ')
+    winners = winners.split(': ')[1].strip().split()
+    picks = picks.strip().split()
     matches = 0
     for p in picks:  # for each number in the picks...
         if p in winners: # ...increment matches if it's also in winners
