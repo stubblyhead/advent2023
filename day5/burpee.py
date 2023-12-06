ea38ef2b496bac2786174789eed30d32997899df
stubblyhead <27823133+stubblyhead@users.noreply.github.com>
2023-12-05 20:29:14 -0800

    basic map class

--- day5/burpee.py
+++ day5/burpee.py
@@ -1,3 +1,19 @@
+class Map:
+    # create object for each line of mappings
+    def __init__(self, map_line):
+        (dest,src,rng) = [ int(i) for i in map_line.split() ]
+        self.dest = dest
+        self.src = src
+        self.rng = rng
+        
+    def convert(self, src):
+        # if source value is in this mapping return the correpsonding destination value,
+        # otherwise return -1
+        if src in range(self.src, self.src + self.rng):
+            return src - self.src + self.dest
+        else:
+            return -1
+
 f = open('input')
 
 parts = f.read().split('\n\n')
@@ -12,7 +28,7 @@ humid_loc = parts[7].split(':\n')[1]
 
 # each hash maps current type to next type
 seeds = {}  # is this a good idea?  time will tell!
-soil = {}  
+soil = {}   # narrator: it was not
 fert = {}
 water = {}
 light = {}
@@ -22,72 +38,3 @@ loc = {}
 
 to_plant = [ int(i) for i in to_plant.split() ]
 
-for p in to_plant:  # need to pre-load each dict with previous things keys
-    seeds[p] = p
-
-for map in seed_soil.split('\n'):
-    (dest, src, rng) = [ int(i) for i in map.split() ]
-    for i in range(rng):  # ${rng} times, conveniently starts at 0
-        seeds[src + i] = dest + i  # src maps to dest
-
-for s in seeds:
-    soil[s] = s
-
-for map in soil_fert.split('\n'):
-    (dest, src, rng) = [ int(i) for i in map.split() ]
-    for i in range(rng):
-        soil[src + i] = dest + i
-
-for s in soil:
-    fert[s] = s
-
-for map in fert_water.split('\n'):
-    (dest, src, rng) = [ int(i) for i in map.split() ]
-    for i in range(rng):
-        fert[src + i] = dest + i
-
-for f in fert:
-    water[f] = f
-
-for map in water_light.split('\n'):
-    (dest, src, rng) = [ int(i) for i in map.split() ]
-    for i in range(rng):
-        water[src + i] = dest + i
-
-for w in water:
-    light[w] = w
-
-for map in light_temp.split('\n'):
-    (dest, src, rng) = [ int(i) for i in map.split() ]
-    for i in range(rng):
-        light[src + i] = dest + i
-
-for l in light:
-    temp[l] = l
-
-for map in temp_humid.split('\n'):
-    (dest, src, rng) = [ int(i) for i in map.split() ]
-    for i in range(rng):
-        temp[src + i] = dest + i
-
-for t in temp:
-    humid[t] = t
-
-for map in humid_loc.split('\n'):
-    (dest, src, rng) = [ int(i) for i in map.split() ]
-    for i in range(rng):
-        humid[src + i] = dest + i
-
-locations = []
-for t in to_plant:
-    t_soil = seeds[t]
-    t_fert = soil[t_soil]
-    t_water = fert[t_fert]
-    t_light = water[t_water]
-    t_temp = light[t_light]
-    t_humid = temp[t_temp]
-    t_loc = humid[t_humid]
-    locations.append(t_loc)
-#    print(f'seed {t} goes in soil {t_soil} which gets fertilizer {t_fert} which gets water {t_water} which gets light {t_light} which should be at temperature {t_temp} and humidity {t_humid} and in location {t_loc}')
-
-print(min(locations))
