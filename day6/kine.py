d5229508ab1abbb22636fdda4a95d95594c064fb
stubblyhead <27823133+stubblyhead@users.noreply.github.com>
2023-12-07 00:44:37 -0800

    need to do something different when time limit is even, but im not doing it right here

---
+++ day6/kine.py
@@ -0,0 +1,34 @@
+from math import ceil, floor
+
+lines = open('testcase')
+
+times = [ int(i) for i in lines.readline().split(':')[1].split() ]
+distances = [ int(i) for i in lines.readline().split(':')[1].split() ]
+
+# basic kinematics, equation is  d = -x^2 + tx (d=distance, t=time)
+# ==> d = -(x - t/2) + (t/2)^2
+# ==> x = t/2 +/- ((t/2)^2 - d)^0.5
+
+timeprod = 1
+
+def min_time(t, d):
+    charge = ceil(t/2 - ((t/2)**2 - d)**0.5)
+    return charge
+
+def max_time(t, d):
+    charge = floor(t/2 +   ((t/2)**2 - d)**0.5)
+    return charge
+
+
+for i in range(len(times)):
+    min_charge = min_time(times[i], distances[i])
+    max_charge = max_time(times[i], distances[i])
+    # have to charge for at least min, but no more than max, so ints 
+    # between the two (inclusive)
+    charge_diff = max_charge - min_charge
+    if charge_diff % 2 == 0:
+        timeprod *= (max_charge - min_charge - 1)
+    else:
+        timeprod *= (max_charge - min_charge + 1)
+
+print(timeprod)
