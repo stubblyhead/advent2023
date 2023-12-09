216193b46ebef282202882a1748010594e15a298
stubblyhead <27823133+stubblyhead@users.noreply.github.com>
2023-12-08 22:25:31 -0800

    testcases passing

---
+++ day8/g-g-g-g-ghosts!!.py
@@ -0,0 +1,28 @@
+from itertools import cycle
+
+with open('testcase2') as f:
+    lines = f.readlines()
+
+dirs = cycle(lines[0].strip())
+
+nodes = {}
+
+for i in lines[2:]: # first line is the directions, next line is just a newline
+    (node, dests) = i.split(' = ')
+    (left, right) = [dests[1:4], dests[6:9]]
+    nodes[node] = [left,right]
+
+stepcount = 0
+
+cur_node = 'AAA'
+
+for n in dirs:
+    stepcount += 1  # increment the number of steps
+    if n == 'L':
+        cur_node = nodes[cur_node][0]  # next node is the left one
+    else:
+        cur_node = nodes[cur_node][1]  # next node is the right one
+    if cur_node == 'ZZZ':
+        break  # we're at the destination, so break out of the loop
+
+print(stepcount)
