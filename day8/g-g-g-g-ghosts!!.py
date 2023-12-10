cc6d1a103c0a458b8b46b44bb8616f4a47b740e7
stubblyhead <27823133+stubblyhead@users.noreply.github.com>
2023-12-09 18:10:32 -0800

    this will be A stepcount where everything ends in Z, but I'm not positive it will be the FIRST time

--- day8/g-g-g-g-ghosts!!.py
+++ day8/g-g-g-g-ghosts!!.py
@@ -1,6 +1,6 @@
 from itertools import cycle
 
-with open('input') as f:
+with open('testcase3') as f:
     lines = f.readlines()
 
 dirs = cycle(lines[0].strip())
@@ -8,23 +8,31 @@ dirs = cycle(lines[0].strip())
 nodes = {}
 
 for i in lines[2:]: # first line is the directions, next line is just a``
-    (node, dests) = i.split(' = ')
-    (left, right) = [dests[1:4], dests[6:9]]
-    nodes[node] = [left,right]
+        (node, dests) = i.split(' = ')
+        (left, right) = [dests[1:4], dests[6:9]]
+        nodes[node] = [left,right]
 
-stepcount = 0
+def get_steps(nodes, dirs, start_node):
+    stepcount = 0
+    cur_node = start_node
+    for n in dirs:
+        stepcount += 1  # increment the number of steps
+        if n == 'L':
+            cur_node = nodes[cur_node][0]  # next node is the left one
+        else:
+            cur_node = nodes[cur_node][1]  # next node is the right one
+        if cur_node[-1] == 'Z':
+            break  # we're at the destination, so break out of the loop
 
-cur_node = 'AAA'
+    return(stepcount)
 
-for n in dirs:
-    stepcount += 1  # increment the number of steps
-    if n == 'L':
-        cur_node = nodes[cur_node][0]  # next node is the left one
-    else:
-        cur_node = nodes[cur_node][1]  # next node is the right one
-    if cur_node == 'ZZZ':
-        break  # we're at the destination, so break out of the loop
-
+stepcount = 1
+for n in nodes.keys():
+    if n[-1] == 'A':
+        thiscount = get_steps(nodes, dirs, n)
+        if n == 'AAA':
+            print(thiscount)
+        stepcount *= thiscount
 print(stepcount)
 
 # stepcount = 0
