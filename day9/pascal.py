c124ce22ba8e34d2a014651635167f46fe07ff95
stubblyhead <27823133+stubblyhead@users.noreply.github.com>
2023-12-09 20:25:35 -0800

    testcase passing

--- day9/pascal.py
+++ day9/pascal.py
@@ -1,13 +1,27 @@
 with open('testcase') as f:
     lines = f.readlines()
 
-def get_diffs(seq):
+def get_diffs(s):
     diffs = []
-    seq.reverse()
-    for i in range(len(seq)-1):
-        diffs.append(seq[i] - seq[i+1])
+    for i in range(len(s)-1):
+        diffs.append(s[i+1] - s[i])
     return diffs
 
-seq = [ int(i) for i in lines[0].split() ]
+sum = 0
 
-print(get_diffs(seq))
+for seq in lines:
+    seq = [ int(i) for i in seq.split() ]
+    layers = [seq]
+    while True:  # don't know how many times we need to do it, remember to break out at some point
+        next_layer = get_diffs(layers[-1])
+        layers.append(next_layer)
+        if next_layer.count(0) == len(next_layer):  # if it's all zeros we're done
+            break
+    layers.pop(-1)  # zeros are extraneous, but will probably be significant laterz
+    layers.reverse()
+    for i in range(len(layers)):
+        if i == len(layers) - 1:
+            sum += layers[i][-1]  # original sequence should have the next number appended
+        else:
+            layers[i+1].append(layers[i+1][-1] + layers[i][-1])
+print(sum)
