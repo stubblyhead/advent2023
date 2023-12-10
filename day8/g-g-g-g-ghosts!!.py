a63d49c997a03b140d0255ac20b0e67fcb01d970
stubblyhead <27823133+stubblyhead@users.noreply.github.com>
2023-12-09 18:03:55 -0800

    i think this will eventually work, but will take a long time

--- day8/g-g-g-g-ghosts!!.py
+++ day8/g-g-g-g-ghosts!!.py
@@ -1,29 +1,52 @@
 from itertools import cycle
 
-with open('testcase3') as f:
+with open('input') as f:
     lines = f.readlines()
 
-# dirs = cycle(lines[0].strip())
+dirs = cycle(lines[0].strip())
 
-# nodes = {}
+nodes = {}
 
-# for i in lines[2:]: # first line is the directions, next line is just a``
-#     (node, dests) = i.split(' = ')
-#     (left, right) = [dests[1:4], dests[6:9]]
-#     nodes[node] = [left,right]
+for i in lines[2:]: # first line is the directions, next line is just a``
+    (node, dests) = i.split(' = ')
+    (left, right) = [dests[1:4], dests[6:9]]
+    nodes[node] = [left,right]
+
+stepcount = 0
+
+cur_node = 'AAA'
+
+for n in dirs:
+    stepcount += 1  # increment the number of steps
+    if n == 'L':
+        cur_node = nodes[cur_node][0]  # next node is the left one
+    else:
+        cur_node = nodes[cur_node][1]  # next node is the right one
+    if cur_node == 'ZZZ':
+        break  # we're at the destination, so break out of the loop
+
+print(stepcount)
 
 # stepcount = 0
 
-# cur_node = 'AAA'
+# cur_nodes = []
+# for n in nodes.keys():
+#     if n[-1] == 'A':
+#         cur_nodes.append(n)  # add nodes to cur_nodes list if last letter is A
 
-# for n in dirs:
+# for d in dirs:
+#     z_count = 0 # keep track of Z-node count in each loop so we don't need to loop through a second time
 #     stepcount += 1  # increment the number of steps
-#     if n == 'L':
-#         cur_node = nodes[cur_node][0]  # next node is the left one
-#     else:
-#         cur_node = nodes[cur_node][1]  # next node is the right one
-#     if cur_node == 'ZZZ':
+#     for i in range(len(cur_nodes)):
+#         if d == 'L':
+#             cur_nodes[i] = nodes[cur_nodes[i]][0]  # next node is the left one            
+#         else:
+#             cur_nodes[i] = nodes[cur_nodes[i]][1]  # next node is the right one
+        
+#         if cur_nodes[i][-1] == 'Z':
+#             z_count += 1
+    
+#     if z_count == len(cur_nodes):
 #         break  # we're at the destination, so break out of the loop
 
 # print(stepcount)
-
