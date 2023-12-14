780fe1205b423a8cca253166468410ae82d6acf8
stubblyhead <27823133+stubblyhead@users.noreply.github.com>
2023-12-13 23:07:05 -0800

    find where springs have to be, might simplify finding the count of possible arrangements

--- day12/picross.py
+++ day12/picross.py
@@ -6,18 +6,19 @@ with open('testcase') as f:
 
 totalcount = 0
 
-def find_springs(pattern, springs):
-    scratch = pattern
+def simplify_pattern(pattern, springs):
     # if a ? must be a # to satisfy conditions, update it as such
-    for i in range(len(springs)):
-        this_length = springs[i]
-        start_pos = find_valid_range(scratch,springs[i])
-        for j in range(start_pos,start_pos + this_length):
-            scratch = scratch[:j] + str(i) + scratch[j+1:]
-        
-        pattern = []
 
-    return left
+    left = right = ''
+    for i in range(len(springs)):
+        for j in range(springs[i]):
+            left += str(i)
+        left += '.'
+
+    if len(left) > len(pattern):
+        left = left[0:len(pattern)]
+    print(left)
+
 
 
 def find_valid_range(pattern, spring_count):
@@ -33,7 +34,7 @@ for l in lines:
     (pattern, springs) = l.split(' ')
     springs = [ int(i) for i in springs.split(',') ]
 
-    find_springs(pattern,springs)
+    simplify_pattern(pattern,springs)
     # knock out the easy ones, not sure how I'm going to approach a general case
 
 
