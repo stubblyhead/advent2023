9322cbe2d3476a84d02de1690c033b0bd3c3c4ef
stubblyhead <27823133+stubblyhead@users.noreply.github.com>
2023-12-15 17:30:01 -0800

    reorganizing a bit

--- day13/idontneednolavasoap.py
+++ day13/idontneednolavasoap.py
@@ -1,40 +1,7 @@
 with open('input') as f:
     patterns = f.read().split('\n\n')
 
-def check_horiz(pattern):
-    for i in range(1,len(pattern)):
-        above = pattern[:i] # split pattern into groups above and below each interstice
-        below = pattern[i:]
-        above.reverse() # will make it easier to iterate through them together
-        mirror = True # assume match until we prove otherwise
-        if len(above) < len(below):
-            for j in range(len(above)):
-                if above[j] == below[j]:
-                    next
-                else:
-                    mirror = False # found rows that don't match...
-                    break # ...so we don't need to keep checking this possible reflection
-        else:
-            for j in range(len(below)):
-                if above[j] == below[j]:
-                    next
-                else:
-                    mirror = False
-                    break
-        if mirror:
-            return 100 * i
-      
-    return -1  # got all the way through and didn't find a reflection
-
-def check_vert(pattern):
-    # easier to transpose once at the start than do it in each iteration
-    tmp = []
-    for i in range(len(pattern[0])):
-        tmp.append( [ l[i] for l in pattern ] )
-    
-    pattern = tmp
-
-    # now basically the same as before but with a different multiplier
+def check_reflection(pattern):
     for i in range(1,len(pattern)):
         above = pattern[:i] # split pattern into groups above and below each interstice
         below = pattern[i:]
@@ -59,16 +26,20 @@ def check_vert(pattern):
       
     return -1  # got all the way through and didn't find a reflection
 
+
 total = 0
 for p in patterns:
     p = p.split()
+    tpose = []
+    for i in range(len(p[0])):
+        tpose.append( [ l[i] for l in p ] )
 
-    h = check_horiz(p)
+    h = check_reflection(p)
     if h > 0:
-        total += h
+        total += 100 * h
         next
     else:
-        v = check_vert(p)
+        v = check_reflection(tpose)
         total += v
 
 print(total)
