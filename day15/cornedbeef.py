7ce1f6598ccd4b4bd63d7b2ea71b12fa01aa4391
stubblyhead <27823133+stubblyhead@users.noreply.github.com>
2023-12-17 12:15:18 -0800

    functionizing hash, working on part 2

--- day15/cornedbeef.py
+++ day15/cornedbeef.py
@@ -1,15 +1,31 @@
+from dataclasses import dataclass
+
 with open('input') as f:
     steps = [ i for i in f.readline().split(',') ]
 
 
+@dataclass
+class Lens:
+    label: str
+    focal_length: int
+
+def get_hash(val):
+    hash = 0
+    for c in val:
+        hash += ord(c)
+        hash *= 17
+        hash %= 256
+
+    return hash
+
 hash_sum = 0
 
 for s in steps:
-    hash = 0
-    for l in s:
-        hash += ord(l)
-        hash *= 17
-        hash %= 256
-    hash_sum += hash
+    hash_sum += get_hash(s)
 
 print(hash_sum)
+
+boxen = [ None for i in range(256) ] # create list with 256 elements
+for s in steps:
+    if s[-1].isnumeric():
+        (label, length) = s.split('=')
