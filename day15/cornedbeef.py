4c8a0f216eb17e26050a13e2e540f5c483e00056
stubblyhead <27823133+stubblyhead@users.noreply.github.com>
2023-12-16 19:20:30 -0800

    reading the instructions is helpful sometimes

--- day15/cornedbeef.py
+++ day15/cornedbeef.py
@@ -2,12 +2,14 @@ with open('input') as f:
     steps = [ i for i in f.readline().split(',') ]
 
 
-hash = 0
+hash_sum = 0
 
 for s in steps:
+    hash = 0
     for l in s:
         hash += ord(l)
         hash *= 17
         hash %= 256
+    hash_sum += hash
 
-print(hash)
+print(hash_sum)
