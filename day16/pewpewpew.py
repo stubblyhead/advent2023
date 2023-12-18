be49998069d45357dcbbf4bb234a80e91743836b
stubblyhead <27823133+stubblyhead@users.noreply.github.com>
2023-12-18 00:26:27 -0800

    not every beam exits, some loop around and around.  count of energized tiles is correct at least.

--- day16/pewpewpew.py
+++ day16/pewpewpew.py
@@ -10,13 +10,13 @@ class Grid:
     def __init__(self, layout):
         self.layout = []
         self.beam_paths = [ [0,0,'R'] ]  # one path to start with, beginning at upper left and moving to the right
-        self.layout[0][0].beam_count = 1
         for i in layout:
             i = i.strip()
             this_row = []
             for j in i:
                 this_row.append(Space(j))
             self.layout.append(this_row)
+        self.layout[0][0].beam_count = 1
         self.height = len(self.layout)
         self.width = len(self.layout[0])
         self.layout[0][0].beams.append('R')  # this might be useful later, not sure yet
@@ -25,7 +25,7 @@ class Grid:
         while self.beam_paths:  # going to assume every path will eventually exit the grid
             to_remove = []  # need to keep track during each tick to not change array length in mid-stream
             to_add = []  # same as above
-            for i in len(self.beam_paths):
+            for i in range(len(self.beam_paths)):
                 p = self.beam_paths[i]
                 # move each beam in the given direction
                 if p[2] == 'U':
@@ -40,12 +40,12 @@ class Grid:
                 if p[0] == -1 or p[0] == self.width \
                 or p[1] == -1 or p[1] == self.height:
                     to_remove.append(i)  # mark for removal from list
-                    next   # move to the next beam
+                    continue   # move to the next beam
                 else:
-                    self.layout[p[0]][p[1]] += 1  # add 1 to beam count for this tile
+                    self.layout[p[0]][p[1]].beam_count += 1  # add 1 to beam count for this tile
                                     
                 # mirrors 
-                if self.layout[p[0]][p[1]] == '/':
+                if self.layout[p[0]][p[1]].tile == '/':
                     if p[2] == 'U':
                         p[2] = 'R'
                     elif p[2] == 'D':
@@ -54,7 +54,7 @@ class Grid:
                         p[2] = 'U'
                     elif p[2] == 'L':
                         p[2] = 'D'
-                elif self.layout[p[0]][p[1]] == '\\':
+                elif self.layout[p[0]][p[1]].tile == '\\':
                     if p[2] == 'U':
                         p[2] = 'L'
                     elif p[2] == 'D':
@@ -65,16 +65,17 @@ class Grid:
                         p[2] = 'U'
 
                 #splitters
-                elif self.layout[p[0]][p[1]] == '-':
+                elif self.layout[p[0]][p[1]].tile == '-':
                     if p[2] == 'U' or p[2] == 'D':
                         p[2] = 'R'  # change direction on one beam...
                         to_add.append(list(p))
-                        to_add[-1][2] == 'L'  # ... and add another beam going in the opposite direction
-                elif self.layout[p[0]][p[1]] == '|':
+                        to_add[-1][2] = 'L'  # ... and add another beam going in the opposite direction
+                elif self.layout[p[0]][p[1]].tile == '|':
                     if p[2] == 'L' or p[2] == 'R':
                         p[2] = 'U'
                         to_add.append(list(p))
-                        to_add[-1][2] == 'D'
+                        to_add[-1][2] = 'D'
+                
 
             to_remove.sort()
             to_remove.reverse() # need to remove from the beam list working backwards or else everything gets fucked up
@@ -82,6 +83,7 @@ class Grid:
                 self.beam_paths.pop(i) # pop out item i
 
             self.beam_paths += to_add  # add new beams on the end
+            print(len(self.beam_paths))
 
 
 with open('testcase') as f:
