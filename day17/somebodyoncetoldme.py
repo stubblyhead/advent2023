35dd888d72d98c44c456c393dcc30f3efc1390a7
stubblyhead <27823133+stubblyhead@users.noreply.github.com>
2023-12-27 19:04:53 -0800

    changed strategy a bit on keeping track of past moves, still getting the same wrong answer

--- day17/somebodyoncetoldme.py
+++ day17/somebodyoncetoldme.py
@@ -1,11 +1,22 @@
 from math import inf
+from dataclasses import dataclass
+@dataclass
+class Leg():
+    remaining: int = 3
+    arrived_heading: str = ''
+
+@dataclass
+class Tile:
+    cost: int
+    leg: Leg
+
 
 class Grid:
     def __init__(self, layout):
 
         self.layout = []
         for i in layout:
-            self.layout.append( [ int(j) for j in list(i.strip())] )
+            self.layout.append( [ Tile(int(j),Leg()) for j in list(i.strip())] )
         self.height = len(self.layout)
         self.width = len(self.layout[0])
 
@@ -40,16 +51,30 @@ class Grid:
             open_set.remove(current)
             cur_row, cur_col = current
             neighbors = self.get_neighbors(cur_row, cur_col)
-            for (n_row, n_col) in neighbors:
-                if len(best_path_to_here[cur_row][cur_col]) > 3:
-                      three_ago_row,three_ago_col = best_path_to_here[cur_row][cur_col][-4]
-                      if (three_ago_row == n_row and abs(three_ago_col - n_col) > 3) \
-                      or (three_ago_col == n_col and abs(three_ago_row - n_row) > 3):
-                          continue # already went 3 in this direction, so we can't go this way
+            for dir in neighbors:
+                # if len(best_path_to_here[cur_row][cur_col]) > 3:
+                #       three_ago_row,three_ago_col = best_path_to_here[cur_row][cur_col][-4]
+                #       if (three_ago_row == n_row and abs(three_ago_col - n_col) > 3) \
+                #       or (three_ago_col == n_col and abs(three_ago_row - n_row) > 3):
+                #           continue # already went 3 in this direction, so we can't go this way
+                n_row, n_col = cur_row, cur_col
+                if dir == 'N':
+                    n_row -= 1
+                elif dir == 'S':
+                    n_row += 1
+                elif dir == 'E':
+                    n_col += 1
+                elif dir == 'W':
+                    n_col -= 1
 
-                tentative_score = cheapest_to_here[cur_row][cur_col] + self.layout[n_row][n_col]
+                tentative_score = cheapest_to_here[cur_row][cur_col] + self.layout[n_row][n_col].cost
                 if tentative_score < cheapest_to_here[n_row][n_col]:
                     cheapest_to_here[n_row][n_col] = tentative_score
+                    self.layout[n_row][n_col].leg.arrived_heading = dir
+                    if self.layout[cur_row][cur_col].leg.arrived_heading == dir:
+                        self.layout[n_row][n_col].leg.remaining = self.layout[cur_row][cur_col].leg.remaining - 1
+                    else:
+                        self.layout[n_row][n_col].leg.remaining = 2
                     best_guess[n_row][n_col] = tentative_score + self.heur(n_row, n_col)
                     best_path_to_here[n_row][n_col] = best_path_to_here[cur_row][cur_col] + [[n_row,n_col]]
                     if [n_row, n_col] not in open_set:
@@ -62,13 +87,20 @@ class Grid:
         return ((self.height - row) + (self.width - col))
     
     def get_neighbors(self,row,col):
-        neighbors = [[row+1,col],[row-1,col],[row,col+1],[row,col-1]]
-        to_remove = []
-        for tile in neighbors:
-            if -1 in tile or tile[0] == self.height or tile[1] == self.width:
-                to_remove.append(tile)
-        for tile in to_remove:
-            neighbors.remove(tile)
+        neighbors = []
+        if row != 0 and self.layout[row][col].leg.arrived_heading != 'S': # not at the top and didn't get here by going south (i.e. not a u-turn)
+            if not (self.layout[row][col].leg.arrived_heading == 'N' and self.layout[row][col].leg.remaining == 0): # haven't just made 3 N moves in a row
+                neighbors.append('N')
+        if col != 0 and self.layout[row][col].leg.arrived_heading != 'E':
+            if not (self.layout[row][col].leg.arrived_heading == 'W' and self.layout[row][col].leg.remaining == 0):
+                neighbors.append('W')
+        if row != self.height - 1 and self.layout[row][col].leg.arrived_heading != 'N':
+            if not (self.layout[row][col].leg.arrived_heading == 'S' and self.layout[row][col].leg.remaining == 0):
+                neighbors.append('S')   
+        if col != self.width -1 and self.layout[row][col].leg.arrived_heading != 'W':
+            if not (self.layout[row][col].leg.arrived_heading == 'E' and self.layout[row][col].leg.remaining == 0):
+                neighbors.append('E')
+
         return neighbors
 
 with open('testcase') as f:
