cf362ce914eeec9f3db29ee40ffc1b0f3ce9ee48
stubblyhead <27823133+stubblyhead@users.noreply.github.com>
2023-12-29 12:39:20 -0800

    fixed parsing of part values

--- day19/xmas.py
+++ day19/xmas.py
@@ -7,6 +7,9 @@ class Part:
     a: int
     s: int
 
+    def sum(h):
+        return h.x+h.m+h.a+h.s
+
 workflows = {}
 
 with open('testcase') as f:
@@ -20,10 +23,51 @@ parts = []
 
 for i in part_list:
     ratings = i.split(',')
-    parts.append(Part(int(ratings[0][3:]),int(ratings[1][3:]),int(ratings[2][3:]),int(ratings[3][3:-1])))
+    parts.append(Part(int(ratings[0][3:]),int(ratings[1][2:]),int(ratings[2][2:]),int(ratings[3][2:-1])))
 
 for i in flow_list:
     label, rules = i.split('{')
     workflows[label] = rules[:-1].split(',')
 
+def do_workflow(w, p):
+    for r in w:
+        if len(r) > 1 and r[1] == '<':
+            cmp,dst = r.split(':')
+            if getattr(p,cmp[0]) < int(cmp[2:]):
+                if dst == 'R':
+                    return 0
+                elif dst == 'A':
+                    return p.sum()
+                else:
+                    return dst
+                
+        elif len(r) > 1 and r[1] == '>':
+            cmp,dst = r.split(':')
+            if getattr(p,cmp[0]) > int(cmp[2:]):
+                if dst == 'R':
+                    return 0
+                elif dst == 'A':
+                    return p.sum()
+                else:
+                    return dst
 
+        elif r == 'A':
+            return p.sum()
+
+        elif r == 'R':
+            return
+
+        else:
+            return r      
+
+
+accepted = 0
+
+for i in parts:
+    w = 'in'
+    while type(w) is str:
+        w = do_workflow(workflows[w], i)
+    if type(w) == int:
+        accepted += w
+
+print(accepted)
