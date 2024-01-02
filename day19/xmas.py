81d4cf5d59bede029bb0fbc384b64aea2e6825a4
stubblyhead <27823133+stubblyhead@users.noreply.github.com>
2024-01-01 21:24:19 -0800

    part 2 complete

--- day19/xmas.py
+++ day19/xmas.py
@@ -13,7 +13,7 @@ class Part:
 
 workflows = {}
 
-with open('testcase') as f:
+with open('input') as f:
     lines = f.read()
 
 flow_list, part_list = lines.split('\n\n')
@@ -87,17 +87,6 @@ class PartRange():
         else:
             return False
 
-    # def __truediv__(self, b):
-    #     if self == b:
-    #         return self
-    #     else:
-    #         if self.min == b.min:
-    #             self.min = b.max + 1
-    #         elif self.max == b.max:
-    #             self.max = b.min - 1
-        
-    #     return PartRange(self.min, self.max)
-
 x = PartRange()
 m = PartRange()
 a = PartRange()
@@ -138,10 +127,5 @@ while not flow_queue.empty():
             valid += (new_x.count * new_m.count * new_a.count * new_s.count)
         elif dest != 'R':
             flow_queue.put([dest,new_x,new_m,new_a,new_s])
-            # x /= new_x
-            # m /= new_m
-            # a /= new_a
-            # s /= new_s
-
             
 print(valid)
